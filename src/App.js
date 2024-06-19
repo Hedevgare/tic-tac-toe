6578803254fbdc4753275c55eedeadaa235f50d6
Edgar Moreira <hedegaremoreira@gmail.com>
2024-06-19 21:23:07 +0100

    Change winner calculation function

--- src/App.js
+++ src/App.js
@@ -17,23 +17,42 @@ export default function Board() {
     }
 
     function calculateWinner(squares) {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
+        const boardSize = 3;
+        for (let i = 0; i < boardSize; i++) {
+            const row = i * boardSize;
+            // Check row
+            if (squares[row] && squares[row] === squares[row + 1] && squares[row] === squares[row + 2]) {
+                return squares[row];
+            }
+            // Check column
+            if (squares[i] && squares[i] === squares[i + boardSize] && squares[i] === squares[i + boardSize * 2]) {
+                return squares[i];
+            }
+            // Check diagonals
+            if (squares[0] && squares[0] === squares[4] && squares[0] === squares[8]) {
+                return squares[0];
+            }
+            if (squares[2] && squares[2] === squares[4] && squares[2] === squares[6]) {
+                return squares[2];
             }
         }
+        // const lines = [
+        //     [0, 1, 2],
+        //     [3, 4, 5],
+        //     [6, 7, 8],
+        //     [0, 3, 6],
+        //     [1, 4, 7],
+        //     [2, 5, 8],
+        //     [0, 4, 8],
+        //     [2, 4, 6],
+        // ];
+
+        // for (let i = 0; i < lines.length; i++) {
+        //     const [a, b, c] = lines[i];
+        //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        //         return squares[a];
+        //     }
+        // }
     }
 
     function checkDraw(squares) {
