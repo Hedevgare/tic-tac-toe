119109c5a516b526b8a628d395d17b101c66df72
Edgar Moreira <hedegaremoreira@gmail.com>
2024-06-19 21:35:02 +0100

    Add winner move highlight

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [winnerMove, setWinnerMove] = useState(Array(3).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
     const [status, setStatus] = useState('Next player: ' + (xIsNext ? 'X' : 'O'));
     const [xScore, setXScore] = useState(0);
@@ -22,37 +23,24 @@ export default function Board() {
             const row = i * boardSize;
             // Check row
             if (squares[row] && squares[row] === squares[row + 1] && squares[row] === squares[row + 2]) {
+                setWinnerMove([row, row + 1, row + 2]);
                 return squares[row];
             }
             // Check column
             if (squares[i] && squares[i] === squares[i + boardSize] && squares[i] === squares[i + boardSize * 2]) {
+                setWinnerMove([i, i + boardSize, i + boardSize * 2]);
                 return squares[i];
             }
             // Check diagonals
             if (squares[0] && squares[0] === squares[4] && squares[0] === squares[8]) {
+                setWinnerMove([0, 4, 8]);
                 return squares[0];
             }
             if (squares[2] && squares[2] === squares[4] && squares[2] === squares[6]) {
+                setWinnerMove([2, 4, 6]);
                 return squares[2];
             }
         }
-        // const lines = [
-        //     [0, 1, 2],
-        //     [3, 4, 5],
-        //     [6, 7, 8],
-        //     [0, 3, 6],
-        //     [1, 4, 7],
-        //     [2, 5, 8],
-        //     [0, 4, 8],
-        //     [2, 4, 6],
-        // ];
-
-        // for (let i = 0; i < lines.length; i++) {
-        //     const [a, b, c] = lines[i];
-        //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        //         return squares[a];
-        //     }
-        // }
     }
 
     function checkDraw(squares) {
@@ -63,6 +51,7 @@ export default function Board() {
         setSquares(Array(9).fill(null));
         setXIsNext(xIsNext ? true : false);
         setStatus('Next player: ' + (xIsNext ? 'X' : 'O'));
+        setWinnerMove(Array(3).fill(null));
     }
 
     function updateStatus(squares, xIsNext) {
@@ -90,19 +79,19 @@ export default function Board() {
             </div>
             <div className="board">
                 <div className="board-row">
-                    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                    <Square isWinner={winnerMove.includes(0)} value={squares[0]} onSquareClick={() => handleClick(0)} />
+                    <Square isWinner={winnerMove.includes(1)} value={squares[1]} onSquareClick={() => handleClick(1)} />
+                    <Square isWinner={winnerMove.includes(2)} value={squares[2]} onSquareClick={() => handleClick(2)} />
                 </div>
                 <div className="board-row">
-                    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+                    <Square isWinner={winnerMove.includes(3)} value={squares[3]} onSquareClick={() => handleClick(3)} />
+                    <Square isWinner={winnerMove.includes(4)} value={squares[4]} onSquareClick={() => handleClick(4)} />
+                    <Square isWinner={winnerMove.includes(5)} value={squares[5]} onSquareClick={() => handleClick(5)} />
                 </div>
                 <div className="board-row">
-                    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                    <Square isWinner={winnerMove.includes(6)} value={squares[6]} onSquareClick={() => handleClick(6)} />
+                    <Square isWinner={winnerMove.includes(7)} value={squares[7]} onSquareClick={() => handleClick(7)} />
+                    <Square isWinner={winnerMove.includes(8)} value={squares[8]} onSquareClick={() => handleClick(8)} />
                 </div>
                 {<div onClick={resetBoard}>
                     <p className="board-reset-btn">Restart</p>
@@ -112,8 +101,9 @@ export default function Board() {
     );
 }
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isWinner }) {
+    const winnerClass = isWinner ? 'winner' : '';
     return (
-        <button className="square" onClick={onSquareClick}>{value}</button>
+        <button className={`square ${winnerClass}`} onClick={onSquareClick}>{value}</button>
     );
 }
